cbf08f32102475d9809fd59aee858b8148efb3e4
yunqian <zxcopen0159@gmail.com>
2021-01-01 20:36:48 +0800

    Fix some feature.

--- Account/logout.php
+++ Account/logout.php
@@ -6,5 +6,5 @@ unset($_SESSION['access_token']);
 $client->revokeToken();
 unset($client);
 // header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //redirect user back to page
-header("Location: login.php");
+header("Location: ../login.php");
 ?>
